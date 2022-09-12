a25cd113bcc50f7e345599cced02730f42370908
Robert Purcell <robert@teamw.net>
2022-09-11 22:15:44 -0400

    Adds a warning option
    Have it do something when a condition is met, but does not fail.

--- src/TodoOrDie/Todo.php
+++ src/TodoOrDie/Todo.php
@@ -7,9 +7,19 @@ use Robertology\TodoOrDie\OverdueError as Exception;
 
 class Todo {
 
+  private string $_message;
+
   public function __construct(string $what_to_do, bool $condition_met) {
     if ($condition_met) {
       throw new Exception($what_to_do);
+    }
+
+    $this->_message = $what_to_do;
+  }
+
+  public function warnIf(bool $condition_met, callable $warn) {
+    if ($condition_met) {
+      $warn($this->_message);
     }
   }
 }
--- test/TodoOrDie/TodoTest.php
+++ test/TodoOrDie/TodoTest.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Robertology\TodoOrDie\Test;
 
+use stdClass;
 use PHPUnit\Framework\TestCase;
 use Robertology\TodoOrDie\ {
   OverdueError as Exception,
@@ -22,6 +23,31 @@ class TodoTest extends TestCase {
     $this->expectExceptionMessage('Some message');
 
     new Todo('Some message', true);
+  }
+
+  public function testItWarns() {
+    $mock = $this->getMockBuilder(stdClass::class)
+      ->addMethods(['sendWarning'])
+      ->getMock();
+
+    $mock->expects($this->once())
+      ->method('sendWarning')
+      ->with('Some message');
+
+    (new Todo('Some message', false))
+      ->warnIf(true, [$mock, 'sendWarning']);
+  }
+
+  public function testItDoesNotWarn() {
+    $mock = $this->getMockBuilder(stdClass::class)
+      ->addMethods(['sendWarning'])
+      ->getMock();
+
+    $mock->expects($this->never())
+      ->method('sendWarning');
+
+    (new Todo('Some message', false))
+      ->warnIf(false, [$mock, 'sendWarning']);
   }
 
 }
